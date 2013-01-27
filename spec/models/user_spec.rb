5ee7c97fbde83c84a3c6a8c179ab133a7beac306
Arie <github@ariekanarie.nl>
2013-01-27 19:54:56 +0100

    specs for find_for_steam_auth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,29 @@ describe User do
 
   end
 
+  describe '.find_for_steam_auth' do
+
+    before do
+      @auth = stub(:provider => 'steam',
+                :uid      => '321',
+                :info     => stub(:name => "Kees", :nickname => "Killer")
+              )
+    end
+
+    it "creates and returns a new user if it can't find an existing one" do
+      expect{User.find_for_steam_auth(@auth)}.to change{User.count}.from(0).to(1)
+    end
+
+    it "returns an existing user if it could find one by uid" do
+      create(:user, :uid => '321')
+      expect{User.find_for_steam_auth(@auth)}.not_to change{User.count}
+    end
+
+    it "updates an existing user with new information" do
+      user = create(:user, :name => "Karel", :uid => '321')
+      expect{User.find_for_steam_auth(@auth)}.not_to change{User.count}
+      user.reload.name.should eql "Kees"
+    end
+  end
+
 end

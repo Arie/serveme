4c3ea2fd882c5cdf9d480defb94c95aafbe80f15
Arie <github@ariekanarie.nl>
2013-02-01 02:27:51 +0100

    oops

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
 
   protect_from_forgery
-  skip_before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
-  def current_user
-    User.first
-  end
 end

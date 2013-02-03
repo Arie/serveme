f65763aa9ce7c448edccaae8e1ecf98fdff60b80
Arie <github@ariekanarie.nl>
2013-02-03 18:00:03 +0100

    cleanup

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -7,10 +7,10 @@ class Statistic
   def self.top_10
     use_count_per_user  = Version.where(:event => 'create').group(:whodunnit).count
     top_10_array        = use_count_per_user.sort_by {|user, count| count }.reverse.first(10)
-    users               = User.scoped
+    top_10_users        = User.where(:id => top_10_array.map(&:first)).to_a
     top_10_hash         = {}
     top_10_array.each do |user_id, count|
-      user = users.select { |u| u.id == user_id.to_i }.first
+      user = top_10_users.select { |u| u.id == user_id.to_i }.first
       top_10_hash[user] = count
     end
     top_10_hash

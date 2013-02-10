4c7c3baa3d867503b24b550d5c8d23845255c8ae
Arie <github@ariekanarie.nl>
2013-02-11 00:19:04 +0100

    should make a git hook for this shit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,6 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
 
   protect_from_forgery
-  skip_before_filter :authenticate_user!
-
-  def current_user
-    User.first
-  end
+  before_filter :authenticate_user!
 
 end

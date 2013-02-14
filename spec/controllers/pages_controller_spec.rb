3e3aaf3a7a75e41cc3144e6e5fb1d7de002553e7
Arie <github@ariekanarie.nl>
2013-02-14 13:34:10 +0100

    specfix

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -24,7 +24,7 @@ describe PagesController do
       sign_in @user
 
       servers = ['foo']
-      Server.should_receive(:reservable_by_user).with(@user).and_return(servers)
+      Server.should_receive(:scoped).and_return(servers)
       get :servers
       assigns(:servers).should eql servers
     end

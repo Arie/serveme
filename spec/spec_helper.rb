7232ac51cb8022a06f9f3e4776a0d370a0d4d9c2
Arie <github@ariekanarie.nl>
2013-02-25 21:14:18 +0100

    also show coverage on command line

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear!
-#
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)

c3dd6abd50fcbebe9f74920347262db921103783
Arie <git@ariekanarie.nl>
2013-05-08 09:31:31 +0200

    order server configs and whitelist by name

--- app/models/whitelist.rb
+++ app/models/whitelist.rb
@@ -2,6 +2,10 @@ class Whitelist < ActiveRecord::Base
   attr_accessible :file
   has_many :reservations, :dependent => :nullify
 
+  def self.ordered
+    order(:file)
+  end
+
   def to_s
     file
   end

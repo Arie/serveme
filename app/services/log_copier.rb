1056454988d670b2fe357c7e0cff29e02f4d7bf1
Arie <git@ariekanarie.nl>
2013-06-16 22:26:35 +0200

    overwrite duplicate logfiles

--- app/services/log_copier.rb
+++ app/services/log_copier.rb
@@ -41,7 +41,7 @@ class SshLogCopier < LogCopier
     zipfile_name_and_path = Rails.root.join("public", "uploads", reservation.zipfile_name)
     Zip::ZipFile.foreach(zipfile_name_and_path) do |zipped_file|
       if zipped_file.name.match("^.*\.log$")
-        zipped_file.extract(File.join(directory_to_copy_to, zipped_file.name))
+        zipped_file.extract(File.join(directory_to_copy_to, zipped_file.name)) { true }
       end
     end
   end

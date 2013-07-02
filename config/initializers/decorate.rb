e2ed4eff6b30b8173b31e79358bb500a6073035b
Arie <git@ariekanarie.nl>
2013-07-02 10:18:12 +0200

    removed unused gem

--- config/initializers/decorate.rb
+++
@@ -1,9 +0,0 @@
-TF2LineParser::Events::Event.types.each do |type|
-  type.class_eval do
-    include Draper::Decoratable
-  end
-end
-
-TF2LineParser::Player.class_eval do
-  include Draper::Decoratable
-end

71be2e6113ff9c91cd014e0f27fd2ba71b416e34
Arie <git@ariekanarie.nl>
2013-08-01 09:54:52 +0200

    NA version can now run master, use an extra worker on na version

--- config/deploy/na.rb
+++ config/deploy/na.rb
@@ -1,7 +1,5 @@
 set :main_server,       "na.fakkelbrigade.eu"
 set :user,              'arie'
-set :puma_flags,        '-w 1 -t 1:8'
-set :branch,            'rcon-ftp-servers'
-
+set :puma_flags,        '-w 2 -t 1:8'
 
 server "#{main_server}", :web, :app, :db, :primary => true

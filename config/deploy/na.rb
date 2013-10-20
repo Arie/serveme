07322ba1293007a389fd1364d4805bee61d4ad04
Arie <git@ariekanarie.nl>
2013-10-20 16:19:27 +0200

    deploy master to NA again

--- config/deploy/na.rb
+++ config/deploy/na.rb
@@ -1,6 +1,5 @@
 set :main_server,       "na.fakkelbrigade.eu"
 set :user,              'arie'
 set :puma_flags,        '-w 2 -t 1:8'
-set :branch,            'na'
 
 server "#{main_server}", :web, :app, :db, :primary => true

d7e3aa91a6a6e447be82036ed12461d8bb7ca55e
Arie <git@ariekanarie.nl>
2014-02-15 14:35:48 +0100

    try 2 sidekiq processes again

--- config/deploy/eu.rb
+++ config/deploy/eu.rb
@@ -2,5 +2,6 @@ set :main_server,       "fakkelbrigade.eu"
 set :user,              'tf2'
 set :puma_socket,       'tcp://127.0.0.1:3010'
 set :puma_flags,        '-w 4 -t 1:8'
+set :sidekiq_processes,  2
 
 server "#{main_server}", :web, :app, :db, :primary => true

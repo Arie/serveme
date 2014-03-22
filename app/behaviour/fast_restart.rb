4569ac1982709dd08fc220b86ad4242efa43740d
Arie <git@ariekanarie.nl>
2014-03-22 22:09:21 +0100

    mapchange has to be a seperate call, else it will always be considered to time out

--- app/behaviour/fast_restart.rb
+++ app/behaviour/fast_restart.rb
@@ -17,7 +17,8 @@ module FastRestart
       unless status.include?("hostname:")
         raise Exception, "RCON status didn't seem correct: #{status}"
       end
-      condenser.rcon_exec("kickall; tftrue_tv_delaymapchange 0; changelevel ctf_turbine")
+      condenser.rcon_exec("kickall; tftrue_tv_delaymapchange 0")
+      condenser.rcon_exec("changelevel ctf_turbine")
     else
       raise Exception, "Couldn't RCON auth to the server"
     end

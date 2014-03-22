abc6732f09f97fe2c79f09e32a839442cecc5a48
Arie <git@ariekanarie.nl>
2014-03-22 20:40:44 +0100

    kickall when fast restarting

--- app/behaviour/fast_restart.rb
+++ app/behaviour/fast_restart.rb
@@ -16,8 +16,7 @@ module FastRestart
       unless status.include?("hostname:")
         raise Exception, "RCON status didn't seem correct: #{status}"
       end
-      rcon_exec("tftrue_tv_delaymapchange 0")
-      rcon_exec("changelevel ctf_turbine")
+      rcon_exec("kickall; tftrue_tv_delaymapchange 0; changelevel ctf_turbine")
     else
       raise Exception, "Couldn't RCON auth to the server"
     end

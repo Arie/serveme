291f2d4d79da0cd09fba6b81d16ae54f4fd01303
Arie <git@ariekanarie.nl>
2014-03-22 16:52:29 +0100

    allow turbine as first map

--- app/validators/reservations/map_is_valid_validator.rb
+++ app/validators/reservations/map_is_valid_validator.rb
@@ -5,8 +5,6 @@ module Reservations
       if record.first_map
         if record.first_map.match(/mvm_.*/)
           record.errors.add(:first_map, "you can't play MvM on our servers")
-        elsif record.first_map == "ctf_turbine"
-          record.errors.add(:first_map, "you can't pick ctf_turbine as first map")
         end
       end
     end

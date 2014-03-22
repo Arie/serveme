8aa88c736a029de28b44304c6d7080361b8ae21a
Arie <git@ariekanarie.nl>
2014-03-22 21:11:17 +0100

    call condensers rcon_exec directly so it raises errors on failure

--- app/behaviour/fast_restart.rb
+++ app/behaviour/fast_restart.rb
@@ -16,7 +16,7 @@ module FastRestart
       unless status.include?("hostname:")
         raise Exception, "RCON status didn't seem correct: #{status}"
       end
-      rcon_exec("kickall; tftrue_tv_delaymapchange 0; changelevel ctf_turbine")
+      condenser.rcon_exec("kickall; tftrue_tv_delaymapchange 0; changelevel ctf_turbine")
     else
       raise Exception, "Couldn't RCON auth to the server"
     end

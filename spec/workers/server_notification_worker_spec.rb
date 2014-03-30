7488b79228d281577a4fe3e3d0f88be301342797
Arie <git@ariekanarie.nl>
2014-03-30 13:44:52 +0200

    specfix

--- spec/workers/server_notification_worker_spec.rb
+++ spec/workers/server_notification_worker_spec.rb
@@ -5,7 +5,7 @@ describe ServerNotificationWorker do
   let(:reservation)   { create :reservation, :starts_at => 10.minutes.ago, :ends_at => 1.hour.from_now, :provisioned => true }
   let(:non_donator)   { create :user }
   let(:donator)       { create :user, :groups => [Group.donator_group] }
-  let(:server)        { double :server }
+  let(:server)        { double(:server, :set_logaddress => true) }
 
   before do
     Reservation.should_receive(:includes).with(:user, :server).and_return(Reservation)
@@ -15,21 +15,35 @@ describe ServerNotificationWorker do
 
   describe "#send_notification" do
 
-    before do
-      create :server_notification, :ad => true, :message => "this is an ad"
+    context "even without notifications" do
+
+      it "sets the logaddress, just in case it was deleted" do
+        server.should_receive(:set_logaddress)
+        ServerNotificationWorker.perform_async
+      end
+
     end
 
-    it "can send an ad to non donators" do
-      reservation.stub(:user => non_donator)
-      server.should_receive(:rcon_say).with("this is an ad")
-      ServerNotificationWorker.perform_async
-    end
+    context "with notifications" do
 
-    it "wont send an add to donators" do
-      create :server_notification, :ad => true, :message => "this is a notification"
-      reservation.stub(:user => donator)
-      server.should_not_receive(:rcon_say).with("this is an ad")
-      ServerNotificationWorker.perform_async
+      before do
+        create :server_notification, :ad => true, :message => "this is an ad"
+      end
+
+
+      it "can send an ad to non donators" do
+        reservation.stub(:user => non_donator)
+        server.should_receive(:rcon_say).with("this is an ad")
+        ServerNotificationWorker.perform_async
+      end
+
+      it "wont send an add to donators" do
+        create :server_notification, :ad => true, :message => "this is a notification"
+        reservation.stub(:user => donator)
+        server.should_not_receive(:rcon_say).with("this is an ad")
+        ServerNotificationWorker.perform_async
+      end
+
     end
 
   end

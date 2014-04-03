0979c27a76e6c155347f06fa0918a7b4a342e270
Arie <git@ariekanarie.nl>
2014-04-03 23:16:46 +0200

    lock na version to ruby 2.0 due to memory usage

--- config/deploy/na.rb
+++ config/deploy/na.rb
@@ -2,6 +2,7 @@ set :main_server,       "na.serveme.tf"
 set :user,              'arie'
 set :puma_flags,        '-w 2 -t 1:8'
 set :sidekiq_processes,  1
+set :rvm_ruby_string,   '2.0.0@serveme'
 
 server "#{main_server}", :web, :app, :db, :primary => true
 

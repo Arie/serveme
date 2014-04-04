86d0f01e1cf87a0ded7e96d21d9c18543fe773e1
Arie <git@ariekanarie.nl>
2014-04-04 03:11:33 +0200

    deploy master branch to eu again

--- config/deploy/eu.rb
+++ config/deploy/eu.rb
@@ -3,6 +3,5 @@ set :user,              'tf2'
 set :puma_socket,       'tcp://127.0.0.1:3010'
 set :puma_flags,        '-w 4 -t 1:8'
 set :sidekiq_processes,  2
-set :branch,            "map_upload"
 
 server "#{main_server}", :web, :app, :db, :primary => true

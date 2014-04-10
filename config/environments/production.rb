62f60507b769e784e5f0eee8a5b7067c2b0cf3fe
Arie <git@ariekanarie.nl>
2014-04-10 16:56:22 +0200

    reenable js uglifying

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Serveme::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.js_compressor = :uglify
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 

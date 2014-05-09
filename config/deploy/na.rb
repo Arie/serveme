929c173c0737ee568dcac3b279498833a35e53c7
Arie <git@ariekanarie.nl>
2014-05-09 11:39:15 +0200

    ruby 2.1.2

--- config/deploy/na.rb
+++ config/deploy/na.rb
@@ -1,7 +1,6 @@
 set :main_server,       "na.serveme.tf"
 set :user,              'arie'
 set :sidekiq_processes,  1
-set :rvm_ruby_string,   '2.0.0@serveme'
 
 server "#{main_server}", :web, :app, :db, :primary => true
 

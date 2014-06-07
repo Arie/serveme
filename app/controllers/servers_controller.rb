a0809d53fc3ec833c74f4fa423a60db0baa4a202
Arie <git@ariekanarie.nl>
2014-06-07 21:08:40 +0200

    disable caching on server page for now

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,7 +1,6 @@
 class ServersController < ApplicationController
 
   skip_before_filter :authenticate_user!
-  caches_action :index, :unless => :current_user
 
   def index
     SteamCondenser::Servers::Sockets::BaseSocket.timeout = 500

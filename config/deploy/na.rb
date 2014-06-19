1d94a95961165745d974f142f4611f06b0db24eb
Arie <git@ariekanarie.nl>
2014-06-18 21:21:07 -0400

    tune down number of sidekiq workers

--- config/deploy/na.rb
+++ config/deploy/na.rb
@@ -1,7 +1,7 @@
 set :main_server,       "na.serveme.tf"
 set :user,              'arie'
-set :sidekiq_processes,  2
-set :puma_flags,        '-w 4 -t 1:8'
+set :sidekiq_processes,  1
+set :puma_flags,        '-w 2 -t 1:8'
 set :rvm_ruby_string,   '2.0.0@serveme'
 
 server "#{main_server}", :web, :app, :db, :primary => true

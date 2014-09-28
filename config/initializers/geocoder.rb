067707b5bc8b7d59e12da298caef0ecfad88e637
Arie <git@ariekanarie.nl>
2014-09-28 11:41:13 +0200

    switch geocoder cache to the rails cache and monkey patch geocoder local lookup to use the cache

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -8,7 +8,7 @@ else
     ip_lookup:        :maxmind_local,
     maxmind_local: {file: Rails.root.join('doc', 'GeoLiteCity.dat') },
     units:            :km,
-    cache:            Redis.new,
+    cache:            Rails.cache,
   )
 end
 Geocoder::Lookup::Test.set_default_stub(
@@ -24,3 +24,40 @@ Geocoder::Lookup::Test.set_default_stub(
     }
   ]
 )
+#Monkeypatch Geocoder so it caches maxmind local lookups
+module Geocoder::Lookup
+  class MaxmindLocal < Base
+    def results(query)
+      if cache and out = cache[query]
+        @cache_hit = true
+      else
+        if configuration[:file]
+          geoip_class = RUBY_PLATFORM == "java" ? JGeoIP : GeoIP
+          result = geoip_class.new(configuration[:file]).city(query.to_s)
+          out = result.nil? ? [] : [result.to_hash]
+        elsif configuration[:package] == :city
+          addr = IPAddr.new(query.text).to_i
+          q = "SELECT l.country, l.region, l.city, l.latitude, l.longitude
+            FROM maxmind_geolite_city_location l WHERE l.loc_id = (SELECT b.loc_id FROM maxmind_geolite_city_blocks b
+            WHERE b.start_ip_num <= #{addr} AND #{addr} <= b.end_ip_num)"
+          out = format_result(q, [:country_name, :region_name, :city_name, :latitude, :longitude])
+        elsif configuration[:package] == :country
+          addr = IPAddr.new(query.text).to_i
+          q = "SELECT country, country_code FROM maxmind_geolite_country
+            WHERE start_ip_num <= #{addr} AND #{addr} <= end_ip_num"
+          out = format_result(q, [:country_name, :country_code2])
+        end
+        cache[query] = out
+        @cache_hit = false
+      end
+      out
+    end
+
+    def cache
+      if @cache.nil? and store = configuration.cache
+        @cache = Geocoder::Cache.new(store, configuration.cache_prefix)
+      end
+      @cache
+    end
+  end
+end

5a801a4a36c26458760faea4bd3a6a0a98654844
Arie <git@ariekanarie.nl>
2014-11-02 11:52:29 +0100

    downgrade au to ruby 2.0 to fix memory issues

--- config/deploy/au.rb
+++ config/deploy/au.rb
@@ -3,5 +3,6 @@ set :user,              'arie'
 set :sidekiq_processes,  1
 set :puma_flags,        '-w 1 -t 1:16'
 set :rvm_type,          :user
+set :rvm_ruby_string,   '2.0.0@serveme'
 
 server "#{main_server}", :web, :app, :db, :primary => true

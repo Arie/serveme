7a65bd209e03c61aaf23747501edd506cef7af6f
Arie <git@ariekanarie.nl>
2014-12-26 00:02:11 +0100

    use correct way to set concurrency for my old capistrano-sidekiq

--- config/deploy/eu.rb
+++ config/deploy/eu.rb
@@ -3,7 +3,7 @@ set :user,              'tf2'
 set :puma_socket,       'tcp://127.0.0.1:3010'
 set :puma_flags,        '-w 4 -t 1:8'
 set :sidekiq_processes,  2
-set :sidekiq_concurrency, 10
+set :sidekiq_options, "-c 10"
 
 server "#{main_server}", :web, :app, :db, :primary => true
 

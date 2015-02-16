6f8722f1ea1ff6ecb018fefcb6942b1c0f3fe786
Arie <git@ariekanarie.nl>
2015-02-16 15:57:53 +0100

    try locking the reservation record so we dont run the same operation multiple times

--- app/workers/reservation_worker.rb
+++ app/workers/reservation_worker.rb
@@ -6,17 +6,19 @@ class ReservationWorker
   attr_accessor :reservation, :reservation_id
 
   def perform(reservation_id, action)
-    begin
-      @reservation_id = reservation_id
-      @reservation = Reservation.find(reservation_id)
-      server = reservation.server
-      server.send("#{action}_reservation", reservation)
-    rescue Exception => exception
-      Rails.logger.error "Something went wrong #{action}-ing the server for reservation #{reservation_id} - #{exception}"
-      Raven.capture_exception(exception) if Rails.env.production?
-    ensure
-      send("after_#{action}_reservation_steps") if reservation
-      Rails.logger.info "#{action.capitalize}ed reservation: #{reservation_id} #{reservation}"
+    @reservation_id = reservation_id
+    @reservation = Reservation.find(reservation_id)
+    @reservation.with_lock do
+      begin
+        server = reservation.server
+        server.send("#{action}_reservation", reservation)
+      rescue Exception => exception
+        Rails.logger.error "Something went wrong #{action}-ing the server for reservation #{reservation_id} - #{exception}"
+        Raven.capture_exception(exception) if Rails.env.production?
+      ensure
+        send("after_#{action}_reservation_steps") if reservation
+        Rails.logger.info "#{action.capitalize}ed reservation: #{reservation_id} #{reservation}"
+      end
     end
   end
 

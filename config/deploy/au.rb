64b1811e924cfc40f552beb2d27cd419ab8a8b9a
Arie <git@ariekanarie.nl>
2015-04-07 21:14:18 +0200

    rip straya

--- config/deploy/au.rb
+++
@@ -1,8 +0,0 @@
-set :main_server,       "direct.au.serveme.tf"
-set :user,              'arie'
-set :sidekiq_processes,  1
-set :puma_flags,        '-w 1 -t 1:16'
-set :rvm_type,          :user
-set :rvm_ruby_string,   '2.0.0@serveme'
-
-server "#{main_server}", :web, :app, :db, :primary => true

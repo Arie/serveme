d1767cee4ea22a424b37d2201568fd5695508e6b
Arie <git@ariekanarie.nl>
2015-04-19 14:33:37 +0200

    order by creation time

--- app/models/reservation_status.rb
+++ app/models/reservation_status.rb
@@ -1,5 +1,9 @@
 class ReservationStatus < ActiveRecord::Base
   belongs_to :reservation
   attr_accessible :status
+
+  def self.ordered
+    order("reservation_statuses.created_at DESC")
+  end
 end
 

3909a62de7c468a79c73ba0f3b0b160f1ca8b551
Arie <git@ariekanarie.nl>
2015-09-07 21:53:15 +0200

    limit execution time of reservation actions

--- app/workers/reservation_worker.rb
+++ app/workers/reservation_worker.rb
@@ -8,17 +8,17 @@ class ReservationWorker
   def perform(reservation_id, action)
     @reservation_id = reservation_id
     @reservation = Reservation.find(reservation_id)
-    @reservation.with_lock do
-      begin
+    begin
+      Timeout::timeout(60) do
         server = reservation.server
         server.send("#{action}_reservation", reservation)
-      rescue Exception => exception
-        Rails.logger.error "Something went wrong #{action}-ing the server for reservation #{reservation_id} - #{exception}"
-        Raven.capture_exception(exception) if Rails.env.production?
-      ensure
-        send("after_#{action}_reservation_steps") if reservation
-        Rails.logger.info "#{action.capitalize}ed reservation: #{reservation}"
       end
+    rescue Exception => exception
+      Rails.logger.error "Something went wrong #{action}-ing the server for reservation #{reservation_id} - #{exception}"
+      Raven.capture_exception(exception) if Rails.env.production?
+    ensure
+      send("after_#{action}_reservation_steps") if reservation
+      Rails.logger.info "#{action.capitalize}ed reservation: #{reservation}"
     end
   end
 

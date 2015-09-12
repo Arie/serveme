c9d7f14f84de2982b25d14f7f21234162fe94f80
Arie <git@ariekanarie.nl>
2015-09-12 15:02:21 +0200

    shell out for local server zipping too

--- app/services/local_zip_file_creator.rb
+++ app/services/local_zip_file_creator.rb
@@ -7,12 +7,7 @@ class LocalZipFileCreator < ZipFileCreator
 
   def zip
     reservation.status_update("Zipping logs and demos of locally running server")
-    Zip::File.open(zipfile_name_and_path, Zip::File::CREATE) do |zipfile|
-      files_to_zip.each do |filename_with_path|
-        filename = File.basename(filename_with_path)
-        zipfile.add(filename, filename_with_path)
-      end
-    end
+    system("zip -j #{zipfile_name_and_path} #{shell_escaped_files_to_zip.join(' ')}")
   end
 
 end
--- spec/services/local_zip_file_creator_spec.rb
+++ spec/services/local_zip_file_creator_spec.rb
@@ -9,14 +9,14 @@ describe LocalZipFileCreator do
   describe '#zip' do
 
     it "it adds the files to zip to a zipfile" do
-      zip_file = LocalZipFileCreator.new(reservation, ["foo'bar"])
-      zip_file.stub(:server => server)
-      zip_file.stub(:files_to_zip => ['foo/qux.zip'])
-      zip_file.stub(:zipfile_name_and_path => 'bar.zip')
-      zip_zip_file = double
-      Zip::File.should_receive(:open).with(zip_file.zipfile_name_and_path, Zip::File::CREATE).and_yield(zip_zip_file)
-      zip_zip_file.should_receive(:add).with('qux.zip', 'foo/qux.zip')
-      zip_file.zip
+      zip_creator = LocalZipFileCreator.new(reservation, ["foo'bar"])
+      zip_creator.stub(:server => server)
+      zip_creator.stub(:files_to_zip => ['foo/qux.dem'])
+      zip_creator.stub(:zipfile_name_and_path => 'bar.zip')
+
+      zip_creator.should_receive(:system).with("zip -j bar.zip foo/qux.dem")
+
+      zip_creator.zip
     end
 
   end

3f13ead2add6911c7d83e429273a88d6b2c51221
Arie <git@ariekanarie.nl>
2015-11-25 23:41:00 +0100

    new instance

--- config/deploy/au.rb
+++ config/deploy/au.rb
@@ -1,4 +1,4 @@
-set :main_server,       "54.66.160.128"
+set :main_server,       "direct.au.serveme.tf"
 set :user,              'arie'
 set :puma_flags,        '-w 2 -t 1:4'
 set :sidekiq_processes,  1
@@ -9,6 +9,6 @@ server "#{main_server}", :web, :app, :db, :primary => true
 namespace :logdaemon do
   desc "Start the logdaemon process"
   task :start, :roles => lambda { roles } do
-    run "cd #{current_path};#{rails_env} #{logdaemon_command} -i 172.31.7.197 -p 40001 -d -l #{logdaemon_log} -P #{logdaemon_pid}"
+    run "cd #{current_path};#{rails_env} #{logdaemon_command} -i 172.31.9.250 -p 40001 -d -l #{logdaemon_log} -P #{logdaemon_pid}"
   end
 end

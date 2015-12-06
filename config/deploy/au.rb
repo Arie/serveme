25b201c03af05d33e3ba36ada291249dc7cf5ea5
Arie <git@ariekanarie.nl>
2015-12-06 13:20:57 +0100

    new AU server

--- config/deploy/au.rb
+++ config/deploy/au.rb
@@ -5,10 +5,3 @@ set :sidekiq_processes,  1
 set :rvm_type,          :user
 
 server "#{main_server}", :web, :app, :db, :primary => true
-
-namespace :logdaemon do
-  desc "Start the logdaemon process"
-  task :start, :roles => lambda { roles } do
-    run "cd #{current_path};#{rails_env} #{logdaemon_command} -i 172.31.9.240 -p 40001 -d -l #{logdaemon_log} -P #{logdaemon_pid}"
-  end
-end

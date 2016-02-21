2fbdd30a2412c87b3a5f73935129052198db6cdc
Arie <git@ariekanarie.nl>
2016-02-21 14:16:53 +0100

    na uses rvm user type now

--- config/deploy/na.rb
+++ config/deploy/na.rb
@@ -2,6 +2,7 @@ set :main_server,       "direct.na.serveme.tf"
 set :user,              'arie'
 set :puma_flags,        '-w 2 -t 1:4'
 set :sidekiq_processes,  1
+set :rvm_type,           :user
 
 server "#{main_server}", :web, :app, :db, :primary => true
 

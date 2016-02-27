62cbd5048e6856be0ac148903612a90848e65e56
Arie <git@ariekanarie.nl>
2016-02-27 13:12:29 +0100

    recognize TF2Pickup reservations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -201,7 +201,7 @@ class Reservation < ActiveRecord::Base
 
   def lobby?
     tags = server.rcon_exec("sv_tags")
-    tags && (tags.include?("TF2Center") || tags.include?("TF2Stadium"))
+    tags && (tags.include?("TF2Center") || tags.include?("TF2Stadium")) || tags.include?("TF2Pickup")
   end
 
   private

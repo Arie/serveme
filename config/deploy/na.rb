a0cf832584186c2dfc691784103f61aed009a3c9
Arie <git@ariekanarie.nl>
2016-03-16 14:33:53 +0100

    new csna server host

--- config/deploy/na.rb
+++ config/deploy/na.rb
@@ -1,9 +1,8 @@
-set :main_server,       "direct.na.serveme.tf"
+set :main_server,       "csna.serveme.tf"
 set :user,              'arie'
 set :puma_flags,        '-w 2 -t 1:4'
 set :sidekiq_processes,  1
 set :rvm_type,           :user
-set :deploy_to,         "/var/www/csgo"
 
 server "#{main_server}", :web, :app, :db, :primary => true
 

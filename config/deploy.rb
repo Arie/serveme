d1227e2ad82c5cc2319b3a8599ca3fcf2036a882
Arie <git@ariekanarie.nl>
2016-09-10 10:46:36 -0400

    update cap version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 require "./config/deploy/logdaemon"
 # config valid only for current version of Capistrano
-lock '3.6.0'
+lock '3.6.1'
 
 set :application,       'serveme'
 set :repo_url,          "https://github.com/Arie/serveme.git"

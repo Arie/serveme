d746b0e75dc350ea526d58f7dfb0e8ade961bb4f
Arie <git@ariekanarie.nl>
2017-02-27 18:19:06 +0100

    Servers page can now be public

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 class ServersController < ApplicationController
 
+  skip_before_filter :authenticate_user!
+
   def index
     SteamCondenser::Servers::Sockets::BaseSocket.timeout = 500
     @servers = Server.active.includes([current_reservations: { :user => :groups } ], :location, :recent_server_statistics).order(:name)

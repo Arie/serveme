158012b68f3d7f8285cbf0fb07ffefd691e86d29
Arie <git@ariekanarie.nl>
2017-03-13 00:08:49 +0100

    Gem updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require './config/deploy/logdaemon'
 # config valid only for current version of Capistrano
-lock '3.7.2'
+lock '3.8.0'
 
 set :application,       'serveme'
 set :repo_url,          'https://github.com/Arie/serveme.git'

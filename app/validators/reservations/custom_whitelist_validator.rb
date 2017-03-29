cf92930af8be0763328f302de9bbc75b7e773bb4
Arie <git@ariekanarie.nl>
2017-03-29 23:41:01 +0200

    Mention whitelist that failed to download in error message

--- app/validators/reservations/custom_whitelist_validator.rb
+++ app/validators/reservations/custom_whitelist_validator.rb
@@ -8,10 +8,10 @@ module Reservations
           begin
             WhitelistTf.download_and_save_whitelist(custom_whitelist_id)
           rescue ActiveRecord::RecordInvalid, Faraday::Error::ClientError
-            record.errors.add(:custom_whitelist_id, "couldn't download the custom whitelist")
+            record.errors.add(:custom_whitelist_id, "couldn't download the custom whitelist: \"#{custom_whitelist_id}\"")
           end
         else
-          record.errors.add(:custom_whitelist_id, "invalid whitelist")
+          record.errors.add(:custom_whitelist_id, "invalid whitelist: \"#{custom_whitelist_id}\"")
         end
       end
     end

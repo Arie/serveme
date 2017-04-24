f4e1f281e99a118088e7544be44ae11f4f55206b
Arie <git@ariekanarie.nl>
2017-04-24 13:29:53 +0200

    remove capistrano lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 require './config/deploy/logdaemon'
-# config valid only for current version of Capistrano
-lock '3.8.0'
 
 set :application,       'serveme'
 set :repo_url,          'https://github.com/Arie/serveme.git'

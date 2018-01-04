561750607d6d220eca92cd2aa1cf32adce010645
Arie <git@ariekanarie.nl>
2018-01-04 15:53:11 +0100

    Use ruby 2.5.0 in prod

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :deploy_to,         '/var/www/serveme'
 set :copy_compression,  :gzip
 set :use_sudo,          false
 set :user,              'tf2'
-set :rvm_ruby_string,   '2.3.0'
+set :rvm_ruby_string,   '2.5.0'
 set :rvm_type,          :system
 
 set :maintenance_template_path, 'app/views/pages/maintenance.html.erb'

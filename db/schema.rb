91e9556bcb461bf53f1f514dc5c790efb6ba2097
Arie <git@ariekanarie.nl>
2018-04-23 16:00:16 +0200

    Rails 5.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170519163845) do
+ActiveRecord::Schema.define(version: 2017_05_19_163845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

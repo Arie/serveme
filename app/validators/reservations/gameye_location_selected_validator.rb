7e98b783b89180dadb7dea9658aff3e52b764c02
Arie <git@ariekanarie.nl>
2019-06-09 15:50:53 +0200

    Support concurrency limit for gameye locations

--- app/validators/reservations/gameye_location_selected_validator.rb
+++ app/validators/reservations/gameye_location_selected_validator.rb
@@ -3,7 +3,12 @@ module Reservations
   class GameyeLocationSelectedValidator < ActiveModel::Validator
 
     def validate(record)
-      unless GameyeServer.location_keys.include?(record.gameye_location)
+      if GameyeServer.location_keys.include?(record.gameye_location)
+        location =  GameyeServer.locations.find {|loc| loc[:id]  == record.gameye_location }
+        if CollisionFinder.new(Reservation.where(:gameye_location => location[:id]), record).colliding_reservations.size >= location[:concurrency_limit]
+          record.errors.add(:gameye_location, "server location is full, please choose another location")
+        end
+      else
         record.errors.add(:gameye_location, "server location is required")
       end
     end

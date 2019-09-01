bb8dbdbb81ab46a35e785f3c0227ff3ae479f365
Arie <git@ariekanarie.nl>
2019-09-02 00:09:55 +0200

    Remove the last few IP bans in case some got themselves banned by spamming the ping refresh button

--- app/models/server.rb
+++ app/models/server.rb
@@ -190,7 +190,7 @@ class Server < ActiveRecord::Base
     end
     if !reservation.server.outdated?
       reservation.status_update("Attempting fast start")
-      if rcon_exec("sv_logsecret #{reservation.logsecret}; logaddress_add direct.#{SITE_HOST}:40001")
+      if rcon_exec("removeip 1; removeip 1; removeip 1; sv_logsecret #{reservation.logsecret}; logaddress_add direct.#{SITE_HOST}:40001")
         first_map = reservation.first_map.presence || "ctf_turbine"
         rcon_exec("changelevel #{first_map}")
         reservation.status_update("Fast start attempted, waiting to boot")

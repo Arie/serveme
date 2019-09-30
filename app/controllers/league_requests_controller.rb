b8c9688ba7c996a1ba32e0590ba97081fb82c6f0
Arie <git@ariekanarie.nl>
2019-09-30 22:30:09 +0200

    Ban dodge searcher for league admins

---
+++ app/controllers/league_requests_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+class LeagueRequestsController < ApplicationController
+  before_action :require_admin_or_streamer
+
+  def new
+    if params[:ip] || params[:steam_uid]
+      @results = LeagueRequest.new(current_user, ip: params[:ip], steam_uid: params[:steam_uid], cross_reference: params[:cross_reference]).search
+      render :index
+    else
+      @league_request = LeagueRequest.new(current_user)
+      render :new
+    end
+  end
+
+  def create
+    @results = LeagueRequest.new(current_user, ip: request_params[:ip], steam_uid: request_params[:steam_uid], cross_reference: request_params[:cross_reference]).search
+    render :index
+  end
+
+  private
+  def request_params
+    params[:league_request].permit([:ip, :steam_uid, :cross_reference])
+  end
+end

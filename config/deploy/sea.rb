ac92ba45b14f825bd46f0a328fa9b45c340abaf2
Arie <git@ariekanarie.nl>
2020-02-05 17:57:55 +0100

    Update SEA deploy configuration

--- config/deploy/sea.rb
+++ config/deploy/sea.rb
@@ -7,3 +7,13 @@ set :rvm_type,           :user
 set :logdaemon_host,     "10.20.0.5"
 
 server "139.99.121.10", user: "serveme", roles: ["web", "app", "db"], port: 50000
+
+namespace :app do
+  desc "symlinks the gameye api token"
+  task :symlink_gameye do
+    on roles(:web, :app) do
+      execute "ln -sf #{shared_path}/config/initializers/gameye.rb #{release_path}/config/initializers/gameye.rb"
+    end
+  end
+end
+after "deploy:symlink:linked_files", "app:symlink_gameye"

e796382f2e39a43af454a72f4fb3e4528efd96fa
Arie <git@ariekanarie.nl>
2020-04-27 13:29:57 +0200

    Throttle

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,8 +1,16 @@
 class Rack::Attack
+  throttle('req/ip', limit: 200, period: 3.minutes) do |req|
+    req.ip
+  end
   throttle('reservations/i_am_feeling_lucky', limit: 5, period: 60.seconds) do |req|
     if req.path == '/reservations/i_am_feeling_lucky' && req.post?
       req.ip
     end
+  end
+  self.throttled_response = lambda do |env|
+    [ 503,  # status
+      {},   # headers
+      ['']] # body
   end
 end
 

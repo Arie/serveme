0d548d90239fca81d38193d8b1c6a91fe7e19be0
Arie <git@ariekanarie.nl>
2020-10-14 19:02:10 +0200

    List files shouldnt receive the tf_dir for NFOServers

--- spec/models/nfo_server_spec.rb
+++ spec/models/nfo_server_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 describe NfoServer do
   describe '#demos' do
     it 'lists all the files and filters the demos' do
-      subject.should_receive(:list_files).with(subject.tf_dir, '') { ['foo.vpk', 'bar.dem', 'gameinfo.txt', 'foo.dem'] }
+      subject.should_receive(:list_files).with('') { ['foo.vpk', 'bar.dem', 'gameinfo.txt', 'foo.dem'] }
       subject.demos.should == ['/tf/bar.dem', '/tf/foo.dem']
     end
   end

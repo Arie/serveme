a7f4a8e517dee360bb6d606bdce4b60873939c54
Arie <git@ariekanarie.nl>
2021-02-01 09:43:53 +0100

    Run with 2 sidekiq processes

--- config/deploy/na.rb
+++ config/deploy/na.rb
@@ -2,7 +2,7 @@ set :main_server,       "direct.na.serveme.tf"
 set :user,              'arie'
 set :puma_threads,      [0,4]
 set :puma_workers,      2
-set :sidekiq_processes,  1
+set :sidekiq_processes,  2
 set :rvm_type,           :user
 
 server "direct.na.serveme.tf", user: "arie", roles: ["web", "app", "db"]

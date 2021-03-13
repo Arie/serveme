408609593e22a75cbc6f650ebd9053e0c63d6608
Arie <git@ariekanarie.nl>
2021-03-13 22:16:30 +0100

    Revert "Move server updates check to cron worker"
    This reverts commit 3d11013e5d290d9d7b8c680931f1a34bab4377cb.

---
+++ app/workers/server_updates_worker.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class ServerUpdatesWorker
+  include Sidekiq::Worker
+
+  def perform
+    latest_version = Server.fetch_latest_version
+    Rails.cache.write('latest_server_version', latest_version) if latest_version
+  end
+end

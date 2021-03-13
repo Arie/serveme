1d5f10f7631160c3d473ace8554c60fb53fa6da4
Arie <git@ariekanarie.nl>
2021-03-13 22:25:37 +0100

    Load jobs and get rid of jobs no longer in the schedule file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 schedule_file = "config/schedule.yml"
 
 if File.exists?(schedule_file) && Sidekiq.server?
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
 end

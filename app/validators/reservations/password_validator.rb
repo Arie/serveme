ee7dc994b10f6dd1420686016c3e981b24392d5e
Arie <git@ariekanarie.nl>
2021-03-25 20:36:54 +0100

    Add password character validation, to prevent broken connect strings

---
+++ app/validators/reservations/password_validator.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Reservations
+  class PasswordValidator < ActiveModel::Validator
+    def validate(record)
+      regex = %r/^[a-zA-Z!@\-\ #$%^&*\/()_+}'|\\:<>?,.\[\]]*$/
+      options[:fields].each do |field|
+        value = record.send(field)
+        next if value.blank? || value.match?(regex)
+
+        record.errors.add(field, 'Invalid characters in password')
+      end
+    end
+  end
+end

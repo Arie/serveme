0af7d03fd2558aacec07d10420832db3f55193ee
Arie <git@ariekanarie.nl>
2021-05-16 20:38:31 +0200

    Rails 6.1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ Serveme::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
   config.cache_store = :mem_cache_store, '127.0.0.1:11211',
-    { :namespace => 'serveme', :expires_in => 1.second, :pool_size => 25 }
+                       { namespace: 'serveme', expires_in: 1.second, pool_size: 25 }
 
   config.eager_load = false
 end

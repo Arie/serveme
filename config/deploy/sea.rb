744019a259afb26f80c49db5734fcaa9e7ef1fdd
Arie <git@ariekanarie.nl>
2021-05-16 14:43:20 +0200

    New SEA host

--- config/deploy/sea.rb
+++ config/deploy/sea.rb
@@ -1,12 +1,11 @@
-set :main_server,       "139.99.121.10"
-set :user,              'serveme'
+set :main_server,       "139.99.42.241"
+set :user,              'arie'
 set :puma_threads,      [0,4]
 set :puma_workers,       1
 set :sidekiq_processes,  1
 set :rvm_type,           :user
-set :logdaemon_host,     "10.20.0.5"
 
-server "139.99.121.10", user: "serveme", roles: ["web", "app", "db"], port: 50000
+server "139.99.42.241", user: "arie", roles: ["web", "app", "db"]
 
 namespace :app do
   desc "symlinks the gameye api token"

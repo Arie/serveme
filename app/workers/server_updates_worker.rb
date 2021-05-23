f15b5533811d88d1f3f97b85cdc131985d6be71f
Arie <git@ariekanarie.nl>
2021-05-24 00:51:30 +0200

    Steam API times out, so expect that

--- app/workers/server_updates_worker.rb
+++ app/workers/server_updates_worker.rb
@@ -7,5 +7,7 @@ class ServerUpdatesWorker
   def perform
     latest_version = Server.fetch_latest_version
     Rails.cache.write('latest_server_version', latest_version) if latest_version
+  rescue Faraday::ConnectionFailed
+    Rails.logger.info 'Failed to fetch latest TF2 version from api.steampowered.com'
   end
 end

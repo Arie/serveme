0140a227ce1f41ca8f23106ea5b7cc343cf13e8d
Arie <git@ariekanarie.nl>
2021-08-10 18:02:54 +0200

    Try new way of enabling mitigations on 1 server

--- app/behaviour/mitigations.rb
+++ app/behaviour/mitigations.rb
@@ -6,22 +6,22 @@ module Mitigations
   end
 
   def enable_mitigations
-    server.execute(
+    server.ssh_exec(
       %(
         sudo iptables -N #{chain_name} &&
-        sudo iptables -A #{chain_name} -p tcp --destination-port #{server.port} -m limit --limit 100/s --limit-burst 100 -j ACCEPT &&
-        sudo iptables -A #{chain_name} -p udp --destination-port #{server.port} -m limit --limit 300/s --limit-burst 600 -j ACCEPT &&
-        sudo iptables -A #{chain_name} -p tcp --destination-port #{server.port} -j DROP &&
-        sudo iptables -A #{chain_name} -p udp --destination-port #{server.port} -j DROP &&
-        sudo iptables -A INPUT -j #{chain_name}
+        sudo iptables -A #{chain_name} -m limit --limit 300/s --limit-burst 300 -j ACCEPT &&
+        sudo iptables -A #{chain_name} -j DROP &&
+        sudo iptables -A INPUT -p udp --destination-port #{server.port} -j #{chain_name} &&
+        sudo iptables -A INPUT -p tcp --destination-port #{server.port} -j #{chain_name}
       )
     )
   end
 
   def disable_mitigations
-    server.execute(
+    server.ssh_exec(
       %(
-        sudo iptables -D INPUT -j #{chain_name} &&
+        sudo iptables -D INPUT -p udp --destination-port #{server.port} -j #{chain_name} &&
+        sudo iptables -D INPUT -p tcp --destination-port #{server.port} -j #{chain_name} &&
         sudo iptables --flush #{chain_name} &&
         sudo iptables -X #{chain_name}
       )
@@ -31,8 +31,7 @@ module Mitigations
   def allow_player(ip)
     server.ssh_exec(
       %(
-        sudo iptables -I #{chain_name} 1 -p tcp -s #{ip} --destination-port #{server.port} -j ACCEPT &&
-        sudo iptables -I #{chain_name} 1 -p udp -s #{ip} --destination-port #{server.port} -j ACCEPT
+        sudo iptables -I #{chain_name} 1 -s #{ip} -j ACCEPT
       )
     )
   end

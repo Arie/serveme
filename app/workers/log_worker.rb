c9c3006b879c516d4bfc528983b5e6b6b4fd4fb2
Arie <git@ariekanarie.nl>
2021-08-21 12:46:33 +0200

    No longer need the anti_dos check, enabled by default now for everyone

--- app/workers/log_worker.rb
+++ app/workers/log_worker.rb
@@ -55,7 +55,7 @@ class LogWorker
     if (ReservationPlayer.banned_uid?(community_id) || ReservationPlayer.banned_ip?(ip)) && !ReservationPlayer.whitelisted_uid?(community_id)
       reservation.server.rcon_exec "banid 0 #{community_id} kick"
       Rails.logger.info "Removed banned player with UID #{community_id}, IP #{event.message}, name #{event.player.name}, from reservation #{reservation_id}"
-    elsif reservation.anti_dos? && reservation.server.supports_mitigations?
+    elsif reservation.server.supports_mitigations?
       rp = ReservationPlayer.where(reservation_id: reservation_id, ip: ip, steam_uid: community_id).first_or_create(name: event.player.name)
       AllowReservationPlayerWorker.perform_async(rp.id)
     end

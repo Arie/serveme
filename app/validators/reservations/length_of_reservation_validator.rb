358a542e58c04def5aa4e9099d3733b2871cdbed
Arie <git@ariekanarie.nl>
2021-09-23 10:10:49 +0200

    Extract method

--- app/validators/reservations/length_of_reservation_validator.rb
+++ app/validators/reservations/length_of_reservation_validator.rb
@@ -3,20 +3,23 @@
 module Reservations
   class LengthOfReservationValidator < ActiveModel::Validator
     def validate(record)
-      user = record.user
-      maximum_reservation_length = if record.gameye?
-                                     3.hours
-                                   else
-                                     user.maximum_reservation_length
-                                   end
+      return unless !record.extending && record.duration.round > maximum_reservation_length(record)
 
-      return unless !record.extending && record.duration.round > maximum_reservation_length
-
-      duration_in_hours = (maximum_reservation_length / 3600.0).round
+      duration_in_hours = (maximum_reservation_length(record) / 3600.0).round
       message = "maximum reservation time is #{duration_in_hours} hours"
 
       message += ', you can extend if you run out of time' unless record.gameye?
       record.errors.add(:ends_at, message)
+    end
+
+    private
+
+    def maximum_reservation_length(record)
+      if record.gameye?
+        3.hours
+      else
+        record.user.maximum_reservation_length
+      end
     end
   end
 end

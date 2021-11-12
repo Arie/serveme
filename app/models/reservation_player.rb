622024925cf299c68a63ffb52e3f5afd22146578
Arie <git@ariekanarie.nl>
2021-11-12 21:49:59 +0100

    Banlist

--- app/models/reservation_player.rb
+++ app/models/reservation_player.rb
@@ -45,7 +45,9 @@ class ReservationPlayer < ActiveRecord::Base
       76_561_199_165_871_973, 76_561_199_164_609_965, 76_561_199_164_115_386, 76_561_199_146_255_686, # Clx
       76_561_199_166_660_740, # Clx
       76_561_198_156_399_565, # Possible Clx alt
-      76_561_198_280_266_851  # Clx leaker Bread
+      76_561_198_280_266_851,  # Clx leaker Bread
+      76_561_197_964_387_679 # 0x0258deaD DDoSer
+
     ].include?(steam_id64.to_i)
   end
 
@@ -61,7 +63,8 @@ class ReservationPlayer < ActiveRecord::Base
       IPAddr.new('162.253.68.0/22'), # Clx
       IPAddr.new('82.222.236.0/22'), # Clx
       IPAddr.new('46.166.176.0/21'), # Clx
-      IPAddr.new('24.133.100.0/22') # Bread
+      IPAddr.new('24.133.100.0/22'), # Bread
+      IPAddr.new('176.40.96.0/21') # 0x0258deaD DDoSer
     ]
   end
 end

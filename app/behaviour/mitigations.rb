4e116b545d5f510110c89dd98030082f57d44c47
Arie <git@ariekanarie.nl>
2021-12-01 10:43:57 +0100

    Use raw table

--- app/behaviour/mitigations.rb
+++ app/behaviour/mitigations.rb
@@ -4,13 +4,13 @@ module Mitigations
   def enable_mitigations
     server.ssh_exec(
       %(
-        sudo iptables -w #{xtables_timeout} -N #{chain_name} &&
-        sudo iptables -w #{xtables_timeout} -A #{chain_name} -p tcp -m limit --limit 100/s --limit-burst 100 -j ACCEPT &&
-        sudo iptables -w #{xtables_timeout} -A #{chain_name} -p udp -m limit --limit 300/s --limit-burst 300 -j ACCEPT &&
-        sudo iptables -w #{xtables_timeout} -A #{chain_name} -j DROP &&
-        sudo iptables -w #{xtables_timeout} -A INPUT -p udp --destination-port #{server.port} -j #{chain_name} &&
-        sudo iptables -w #{xtables_timeout} -A INPUT -p tcp --destination-port #{server.port} -j #{chain_name} &&
-        sudo iptables -w #{xtables_timeout} -I #{chain_name} 1 -s direct.#{SITE_HOST} -j ACCEPT -m comment --comment "#{chain_name}-system"
+        sudo iptables -w #{xtables_timeout} -N #{chain_name} -t raw &&
+        sudo iptables -w #{xtables_timeout} -A #{chain_name} -t raw -p tcp -m limit --limit 100/s --limit-burst 100 -j ACCEPT &&
+        sudo iptables -w #{xtables_timeout} -A #{chain_name} -t raw -p udp -m limit --limit 300/s --limit-burst 300 -j ACCEPT &&
+        sudo iptables -w #{xtables_timeout} -A #{chain_name} -t raw -j DROP &&
+        sudo iptables -w #{xtables_timeout} -A PREROUTING -t raw -p udp --destination-port #{server.port} -j #{chain_name} &&
+        sudo iptables -w #{xtables_timeout} -A PREROUTING -t raw -p tcp --destination-port #{server.port} -j #{chain_name} &&
+        sudo iptables -w #{xtables_timeout} -I #{chain_name} -t raw 1 -s direct.#{SITE_HOST} -j ACCEPT -m comment --comment "#{chain_name}-system"
       ), log_stderr: true
     )
   end
@@ -18,12 +18,12 @@ module Mitigations
   def enable_sdr_mitigations
     server.ssh_exec(
       %(
-        sudo iptables -w #{xtables_timeout} -N #{chain_name} &&
-        sudo iptables -w #{xtables_timeout} -A #{chain_name} -p tcp -m limit --limit 100/s --limit-burst 100 -j ACCEPT &&
-        sudo iptables -w #{xtables_timeout} -A #{chain_name} -j DROP &&
-        sudo iptables -w #{xtables_timeout} -A INPUT -p udp --destination-port #{server.port} -j #{chain_name} &&
-        sudo iptables -w #{xtables_timeout} -A INPUT -p tcp --destination-port #{server.port} -j #{chain_name} &&
-        sudo iptables -w #{xtables_timeout} -I #{chain_name} 1 -s direct.#{SITE_HOST} -j ACCEPT -m comment --comment "#{chain_name}-system"
+        sudo iptables -w #{xtables_timeout} -N #{chain_name} -t raw &&
+        sudo iptables -w #{xtables_timeout} -A #{chain_name} -t raw -p tcp -m limit --limit 100/s --limit-burst 100 -j ACCEPT &&
+        sudo iptables -w #{xtables_timeout} -A #{chain_name} -t raw -j DROP &&
+        sudo iptables -w #{xtables_timeout} -A PREROUTING -t raw -p udp --destination-port #{server.port} -j #{chain_name} &&
+        sudo iptables -w #{xtables_timeout} -A PREROUTING -t raw -p tcp --destination-port #{server.port} -j #{chain_name} &&
+        sudo iptables -w #{xtables_timeout} -I #{chain_name} 1 -t raw -s direct.#{SITE_HOST} -j ACCEPT -m comment --comment "#{chain_name}-system"
       ), log_stderr: true
     )
   end
@@ -31,10 +31,10 @@ module Mitigations
   def disable_mitigations(log_stderr: true)
     server.ssh_exec(
       %(
-        sudo iptables -w #{xtables_timeout} -D INPUT -p udp --destination-port #{server.port} -j #{chain_name} &&
-        sudo iptables -w #{xtables_timeout} -D INPUT -p tcp --destination-port #{server.port} -j #{chain_name} &&
-        sudo iptables -w #{xtables_timeout} --flush #{chain_name} &&
-        sudo iptables -w #{xtables_timeout} -X #{chain_name}
+        sudo iptables -w #{xtables_timeout} -D PREROUTING -t raw -p udp --destination-port #{server.port} -j #{chain_name} &&
+        sudo iptables -w #{xtables_timeout} -D PREROUTING -t raw -p tcp --destination-port #{server.port} -j #{chain_name} &&
+        sudo iptables -w #{xtables_timeout} -t raw --flush #{chain_name} &&
+        sudo iptables -w #{xtables_timeout} -t raw -X #{chain_name}
       ), log_stderr: log_stderr
     )
   end
@@ -43,7 +43,7 @@ module Mitigations
     if reservation_player.duplicates.whitelisted.none?
       server.ssh_exec(
         %(
-          sudo iptables -w #{xtables_timeout} -I #{chain_name} 1 -s #{reservation_player.ip} -j ACCEPT -m comment --comment "#{chain_name}-#{reservation_player.steam_uid}"
+          sudo iptables -w #{xtables_timeout} -I #{chain_name} 1 -t raw -s #{reservation_player.ip} -j ACCEPT -m comment --comment "#{chain_name}-#{reservation_player.steam_uid}"
         ), log_stderr: true
       )
     end

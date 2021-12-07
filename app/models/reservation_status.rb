8f5cde7d7365f84f3e2ef0e35f04860d3dcace8d
Arie <git@ariekanarie.nl>
2021-12-07 22:48:42 +0100

    Hotwire experiment

--- app/models/reservation_status.rb
+++ app/models/reservation_status.rb
@@ -3,6 +3,8 @@
 class ReservationStatus < ActiveRecord::Base
   belongs_to :reservation
 
+  after_create_commit -> { broadcast_prepend_to reservation }
+
   def self.ordered
     order('reservation_statuses.created_at DESC')
   end

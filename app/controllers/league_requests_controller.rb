888dd7f4f705a0c5252520ec0f0e270d78d144fc
Arie <git@ariekanarie.nl>
2021-12-08 11:19:32 +0100

    Fix the league request search and hotwire it

--- app/controllers/league_requests_controller.rb
+++ app/controllers/league_requests_controller.rb
@@ -4,18 +4,32 @@ class LeagueRequestsController < ApplicationController
   before_action :require_site_or_league_admin
 
   def new
-    if params[:ip] || params[:steam_uid]
-      @results = LeagueRequest.new(current_user, ip: params[:ip], steam_uid: params[:steam_uid], cross_reference: params[:cross_reference]).search
-      render :index
-    else
-      @league_request = LeagueRequest.new(current_user)
-      render :new
+    respond_to do |format|
+      if params[:ip] || params[:steam_uid]
+        @results = LeagueRequest.new(current_user, ip: params[:ip], steam_uid: params[:steam_uid], cross_reference: params[:cross_reference]).search
+        if @results
+          format.html { render :index }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+        end
+      else
+        format.html do
+          @league_request = LeagueRequest.new(current_user)
+          render :new
+        end
+      end
     end
   end
 
   def create
-    @results = LeagueRequest.new(current_user, ip: request_params[:ip], steam_uid: request_params[:steam_uid], cross_reference: request_params[:cross_reference]).search
-    render :index
+    respond_to do |format|
+      @results = LeagueRequest.new(current_user, ip: request_params[:ip], steam_uid: request_params[:steam_uid], cross_reference: request_params[:cross_reference]).search
+      if @results
+        format.html { render :index }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
   end
 
   private

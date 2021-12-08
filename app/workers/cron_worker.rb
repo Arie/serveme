92d10f649296ef477797c9ec1f20f2f931a19105
Arie <git@ariekanarie.nl>
2021-12-08 20:25:43 +0100

    Update admin server list same way as for normal users

--- app/workers/cron_worker.rb
+++ app/workers/cron_worker.rb
@@ -51,5 +51,6 @@ class CronWorker
   def update_servers_page
     servers = Server.active.includes([current_reservations: { user: :groups }], :location, :recent_server_statistics).order(:name)
     Turbo::StreamsChannel.broadcast_replace_to 'server-list', target: 'server-list', partial: 'servers/list', locals: { servers: servers }
+    Turbo::StreamsChannel.broadcast_replace_to 'admin-server-list', target: 'admin-server-list', partial: 'servers/admin_list', locals: { servers: servers }
   end
 end

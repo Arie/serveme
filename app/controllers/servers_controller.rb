93daf2d659b7fcad805b34722ac94b4416dcd63a
Arie <git@ariekanarie.nl>
2021-12-08 20:12:48 +0100

    Make the server page update itself

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -2,7 +2,11 @@
 
 class ServersController < ApplicationController
   def index
-    SteamCondenser::Servers::Sockets::BaseSocket.timeout = 500
     @servers = Server.active.includes([current_reservations: { user: :groups }], :location, :recent_server_statistics).order(:name)
+    if current_admin || current_league_admin || current_streamer
+      render :admins
+    else
+      render :index
+    end
   end
 end
--- app/models/server_statistic.rb
+++ app/models/server_statistic.rb
@@ -3,4 +3,6 @@
 class ServerStatistic < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :server
+
+  after_create_commit -> { broadcast_replace_to server, target: server, partial: 'servers/server_admin_info', locals: { server: server } }
 end

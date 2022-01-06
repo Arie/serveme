c4ea03561a5740b9e468e6089ef0f9d2b1853505
Arie <git@ariekanarie.nl>
2022-01-06 21:43:41 +0100

    Turn auto completer into a real object

--- spec/services/rcon_autocomplete_spec.rb
+++ spec/services/rcon_autocomplete_spec.rb
@@ -3,17 +3,22 @@
 require 'spec_helper'
 
 describe RconAutocomplete do
+  subject { described_class.new(nil) }
+
   it 'returns a list of suggestions' do
-    expect(described_class.autocomplete('ban')).to start_with %w[banid banip]
-    expect(described_class.autocomplete('tf_bot')).to start_with %w[tf_bot_add tf_bot_difficulty tf_bot_kick tf_bot_kill]
+    expect(subject.autocomplete('ban')).to start_with %w[banid banip]
+    expect(subject.autocomplete('tf_bot')).to start_with %w[tf_bot_add tf_bot_difficulty tf_bot_kick tf_bot_kill]
   end
 
   it 'limits auto completion to 5 suggestions' do
-    expect(described_class.autocomplete('tf_').length).to be(5)
+    expect(subject.autocomplete('tf_').length).to be(5)
   end
 
-  it 'completes certain commands further' do
-    expect(described_class.autocomplete('changelevel cp_r')).to start_with ['changelevel cp_reckoner_rc6']
-    expect(described_class.autocomplete('exec etf')).to start_with ['exec etf2l', 'exec etf2l_6v6']
+  it 'completes changelevel further' do
+    expect(subject.autocomplete('changelevel cp_r')).to start_with ['changelevel cp_reckoner_rc6']
+  end
+
+  it 'completes exec further' do
+    expect(subject.autocomplete('exec etf')).to start_with ['exec etf2l', 'exec etf2l_6v6']
   end
 end

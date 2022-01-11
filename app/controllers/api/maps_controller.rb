b7bd619839b9859b3bec75da0723c63802cac61e
Arie <git@ariekanarie.nl>
2022-01-11 11:09:24 +0100

    Make maps listing public in API

--- app/controllers/api/maps_controller.rb
+++ app/controllers/api/maps_controller.rb
@@ -2,6 +2,8 @@
 
 module Api
   class MapsController < Api::ApplicationController
+    skip_before_action :verify_api_key
+
     def index
       @maps = MapUpload.available_maps
       @cloud_maps = MapUpload.available_cloud_maps

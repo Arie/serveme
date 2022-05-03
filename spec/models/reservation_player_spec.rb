9bfe38537748e68b0c6a8cb9e0134d3dac816126
Arie <git@ariekanarie.nl>
2022-05-03 14:14:35 +0200

    Use CSVs to store the bans and whitelist

--- spec/models/reservation_player_spec.rb
+++ spec/models/reservation_player_spec.rb
@@ -12,4 +12,32 @@ describe ReservationPlayer do
       expect(described_class.banned_asn?('1.128.0.1')).to be true
     end
   end
+
+  context 'banned steam ids' do
+    it 'recognizes a banned steam id' do
+      expect(described_class.banned_uid?(76561198310925535)).to be true
+    end
+    it 'doesnt flag a good steam id as banned' do
+      expect(described_class.banned_uid?(76561197960497430)).to be false
+    end
+  end
+
+  context 'whitelisted steam id' do
+    it 'recognizes a whitelisted steam id' do
+      expect(described_class.whitelisted_uid?(76561198360811196)).to be true
+    end
+
+    it 'doesnt flag an unknown steam id as whitelisted' do
+      expect(described_class.whitelisted_uid?('foobarwidget')).to be false
+    end
+  end
+
+  context 'banned ip' do
+    it 'recognizes a banned ip in a range' do
+      expect(described_class.banned_ip?('87.249.134.123')).to be true
+    end
+    it 'doesnt flag a good ip as banned' do
+      expect(described_class.banned_ip?('127.0.0.1')).to be false
+    end
+  end
 end

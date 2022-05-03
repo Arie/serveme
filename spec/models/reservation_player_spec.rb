639fac6dc13ea399cf50f2d11bd5f27ba44acc56
Arie <git@ariekanarie.nl>
2022-05-03 13:06:25 +0200

    Use MaxMind ASN database

--- spec/models/reservation_player_spec.rb
+++ spec/models/reservation_player_spec.rb
@@ -2,15 +2,14 @@ require 'spec_helper'
 
 describe ReservationPlayer do
   context 'banned asns' do
-    it 'recognizes bad ASNs' do
-      ['87.249.134.16', '91.245.254.68'].each do |vpn_ip|
-        expect(described_class.banned_asn?(vpn_ip)).to be true
-      end
+    it 'doesnt flag good ASNs' do
+      good_ip = '1.128.0.1'
+      expect(described_class.banned_asn?(good_ip)).to be false
     end
 
-    it 'doesnt flag good ASNs' do
-      good_ip = '213.46.237.24'
-      expect(described_class.banned_asn?(good_ip)).to be false
+    it 'recognizes bad ASNs' do
+      described_class.stub(:custom_banned_asns).and_return([1221])
+      expect(described_class.banned_asn?('1.128.0.1')).to be true
     end
   end
 end

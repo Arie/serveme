9f48239f9cd944d3ddbd24c8b1903a189fd0b8ac
Arie <git@ariekanarie.nl>
2022-05-03 14:14:35 +0200

    Use CSVs to store the bans and whitelist

--- app/models/reservation_player.rb
+++ app/models/reservation_player.rb
@@ -29,61 +29,27 @@ class ReservationPlayer < ActiveRecord::Base
   def self.whitelisted_uid?(steam_id64)
     return true unless steam_id64
 
-    [
-      76561198194941135, # Despair, shares ISP with Gremlin
-      76561198253170562, # Shock, shares ISP with Gremlin
-      76561198378191199, # Sori666, shares ISP with Gremlin
-      76561198360811196, # Sun Tzu, shares ISP with Clx
-      76561198238943688, # Flewvar, shares ISP with Clx
-      76561198167849935, # Laggy Lanny, shares IP with Gremlin
-      76561198210526425  # Still, shares IP with Gremlin
-    ].include?(steam_id64.to_i)
+    whitelisted_uids.include?(steam_id64.to_i)
+  end
+
+  def self.whitelisted_uids
+    @whitelisted_uids ||= CSV.read(Rails.root.join('doc', 'whitelisted_steam_ids.csv'), headers: true).map { |row| row['steam_id64'].to_i }
   end
 
   def self.banned_uid?(steam_id64)
-    [
-      76561198310925535, 76561199116364920, 76561198113294936, # Clx
-      76561199165871973, 76561199164609965, 76561199164115386, 76561199146255686, # Clx
-      76561199166660740, 76561199135772351, # Clx
-      76561198156399565, # Possible Clx alt
-      76561198280266851, # Clx leaker Bread
-      76561197964387679, # 0x0258deaD DDoSer
-      76561199186114313, # Impersonating serveme.tf personnel
-      76561199062609974, 76561198238170280, 76561199208354375, 76561199199088995, # Cheeto match invader
-      76561198091464403, 76561199132066910, 76561197962267804, 76561199200781287, # Cheeto match invader
-      76561198347491669, 76561199162197644, 76561199074642692, 76561199275915828, # Cheeto match invader
-      76561199251574288, 76561198091464403, # semiperf log spammer
-      76561198167446102, 76561198081019811, 76561199129719751, 76561199178857855, # sandstoner log spammer
-      76561199133700932, 76561198035013366 # sandstoner log spammer
-    ].include?(steam_id64.to_i)
+    banned_uids.include?(steam_id64)
   end
 
   def self.banned_ip?(ip)
     banned_ranges.any? { |range| range.include?(ip) }
   end
 
+  def self.banned_uids
+    @banned_uids ||= CSV.read(Rails.root.join('doc', 'banned_steam_ids.csv'), headers: true).map { |row| row['steam_id64'].to_i }
+  end
+
   def self.banned_ranges
-    @banned_ranges ||= [
-      IPAddr.new('107.181.180.0/24'), # Clx
-      IPAddr.new('162.253.68.0/22'), # Clx
-      IPAddr.new('82.222.236.0/22'), # Clx
-      IPAddr.new('46.166.176.0/21'), # Clx
-      IPAddr.new('45.89.173.0/24'), # Clx
-      IPAddr.new('24.133.100.0/22'), # Bread
-      IPAddr.new('176.40.96.0/21'), # 0x0258deaD DDoSer
-      IPAddr.new('69.247.46.46/32'), # Cheeto match invader
-      IPAddr.new('198.54.128.0/24'), # Cheeto match invader
-      IPAddr.new('45.134.142.0/24'), # Cheeto match invader
-      IPAddr.new('94.198.42.0/24'), # Cheeto match invader
-      IPAddr.new('193.27.12.0/24'), # Cheeto match invader
-      IPAddr.new('89.45.224.0/24'), # Cheeto match invader
-      IPAddr.new('87.249.134.0/24'), # Cheeto match invader
-      IPAddr.new('75.131.150.35/32'), # semiperf log spammer
-      IPAddr.new('68.116.180.185/32'), # semiperf log spammer
-      IPAddr.new('75.131.148.146/32'), # semiperf log spammer
-      IPAddr.new('73.55.161.142/32'), # sandstoner log spammer
-      IPAddr.new('24.200.212.144/32') # sandstoner log spammer
-    ]
+    @banned_ranges ||= CSV.read(Rails.root.join('doc', 'banned_ips.csv'), headers: true).map { |row| IPAddr.new(row['ip']) }
   end
 
   def self.banned_asn?(ip)

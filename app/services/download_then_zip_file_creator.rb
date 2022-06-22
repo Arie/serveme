6517d0ace75517f3ccd8250fd002067e2a45ee3e
Arie <git@ariekanarie.nl>
2022-06-22 11:30:22 +0200

    Support scrubbing of logs.tf API key from TFTrue and logstf.smx

--- app/services/download_then_zip_file_creator.rb
+++ app/services/download_then_zip_file_creator.rb
@@ -16,7 +16,7 @@ class DownloadThenZipFileCreator < ZipFileCreator
   end
 
   def strip_ips_and_api_keys_from_log_files(tmp_dir)
-    strip_command = %q|LANG=ALL LC_ALL=C sed -i -r 's/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b/0.0.0.0/g;s/tftrue_logs_apikey \"\S+\"/tftrue_logs_apikey \"apikey\"/g;s/sm_demostf_apikey \"\S+\"/sm_demostf_apikey \"apikey\"/g'|
+    strip_command = %q|LANG=ALL LC_ALL=C sed -i -r 's/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b/0.0.0.0/g;s/logstf_apikey \"\S+\"/logstf_apikey \"apikey\"/g;s/tftrue_logs_apikey \"\S+\"/tftrue_logs_apikey \"apikey\"/g;s/sm_demostf_apikey \"\S+\"/sm_demostf_apikey \"apikey\"/g'|
     strip_files   = "#{tmp_dir}/*.log"
     system("#{strip_command} #{strip_files}")
   end

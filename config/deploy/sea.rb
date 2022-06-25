f385bfaa5de6a8bee666d462eeda01813f76878a
Arie <git@ariekanarie.nl>
2022-06-25 15:16:15 +0200

    Disable PTY for SEA server, so sidekiq runs

--- config/deploy/sea.rb
+++ config/deploy/sea.rb
@@ -4,5 +4,6 @@ set :puma_threads,      [0,4]
 set :puma_workers,       1
 set :sidekiq_processes,  1
 set :rvm_type,           :user
+set :pty,                false
 
 server "139.99.42.241", user: "arie", roles: ["web", "app", "db"]

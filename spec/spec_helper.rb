1488cd48b1dff9da5852fa6a6c7c7e83ab9fa235
Arie <git@ariekanarie.nl>
2022-07-26 00:20:02 +0200

    Ruby 3.1.2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,6 @@ require 'sidekiq'
 require 'sidekiq/testing'
 Sidekiq::Testing.inline!
 
-SimpleCov.start 'rails'
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 gem 'minitest'

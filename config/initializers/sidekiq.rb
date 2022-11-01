49cbcdc6cf27a3df6ecc1b21fef57c3d6f57d837
Arie <git@ariekanarie.nl>
2022-11-01 21:15:20 +0100

    Downgrade sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,4 @@
 schedule_file = "config/schedule.yml"
-Sidekiq.strict_args!(false)
 
 if File.exists?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)

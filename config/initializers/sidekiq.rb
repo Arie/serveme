7e5baab57fb0092076f6c2de13c6a62dd528f8de
Arie <git@ariekanarie.nl>
2022-11-01 21:09:04 +0100

    Gem updates

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,5 @@
 schedule_file = "config/schedule.yml"
+Sidekiq.strict_args!(false)
 
 if File.exists?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)

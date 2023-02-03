4e7e8463a73171f110b2e1485b787b8078bcb9e1
Arie <git@ariekanarie.nl>
2023-02-03 19:01:24 +0100

    Update stripe yaml loader to allow aliases

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
+
 stripe_file = 'config/stripe.yml'
 
 if File.exist?(stripe_file)
-  stripe_config = YAML.load_file(stripe_file).fetch(Rails.env.to_s) {}
+  stripe_config = YAML.load_file(stripe_file, aliases: true).fetch(Rails.env.to_s) {}
   Stripe.api_key = stripe_config.fetch('api_key') {}
   STRIPE_PUBLISHABLE_KEY = stripe_config.fetch('publishable_key')
 end

8eab6206d7c57f7488ef7a0d4acab2cbcc61ec77
Arie <git@ariekanarie.nl>
2023-02-04 14:01:16 +0100

    Check for prerouting rule before trying to add a new one

--- app/behaviour/mitigations.rb
+++ app/behaviour/mitigations.rb
@@ -4,17 +4,14 @@ module Mitigations
   def enable_mitigations
     server.ssh_exec(
       %(
-        #{iptables} -D PREROUTING -p udp -m udp --dport 27015 -j NOTRACK;
-        #{iptables} -I PREROUTING -p udp -m udp --dport 27015 -j NOTRACK &&
-        #{iptables} -D PREROUTING -t raw -p udp --destination-port #{server.port} -j #{chain_name};
-        #{iptables} -D PREROUTING -t raw -p tcp --destination-port #{server.port} -j #{chain_name};
+        #{iptables} -C PREROUTING -p udp -m udp --dport 27015 -j NOTRACK || #{iptables} -I PREROUTING -p udp -m udp --dport 27015 -j NOTRACK;
         #{iptables} -t raw --flush #{chain_name};
         #{iptables} -N #{chain_name} -t raw;
-        #{iptables} -A #{chain_name} -t raw -p tcp -m limit --limit 100/s --limit-burst 100 -j ACCEPT &&
+        #{iptables} -A #{chain_name} -t raw -p tcp -m limit --limit 100/s --limit-burst 100 -j ACCEPT;
         #{allow_limited_udp_rule}
-        #{iptables} -A #{chain_name} -t raw -j DROP &&
-        #{iptables} -A PREROUTING -t raw -p udp --destination-port #{server.port} -j #{chain_name} &&
-        #{iptables} -A PREROUTING -t raw -p tcp --destination-port #{server.port} -j #{chain_name} &&
+        #{iptables} -A #{chain_name} -t raw -j DROP;
+        #{iptables} -C PREROUTING -t raw -p udp --destination-port #{server.port} -j #{chain_name} || #{iptables} -A PREROUTING -t raw -p udp --destination-port #{server.port} -j #{chain_name};
+        #{iptables} -C PREROUTING -t raw -p tcp --destination-port #{server.port} -j #{chain_name} || #{iptables} -A PREROUTING -t raw -p tcp --destination-port #{server.port} -j #{chain_name};
         #{iptables} -I #{chain_name} 1 -t raw -s direct.#{SITE_HOST} -j ACCEPT -m comment --comment "#{chain_name}-system"
       ), log_stderr: false
     )

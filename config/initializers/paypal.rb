025429740d41a8b018811a72a782933314522727
Arie <git@ariekanarie.nl>
2023-02-14 21:34:18 +0100

    Disable paypal logging below warning level in test

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,1 +1,2 @@
-PayPal::SDK::Core::Config.load('config/paypal.yml',  Rails.env)
+PayPal::SDK::Core::Config.load('config/paypal.yml', Rails.env)
+PayPal::SDK.logger.level = Logger::WARN if Rails.env.test?

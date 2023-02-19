e6d137cc97e5253a56022acd73f9531f0822c930
Arie <git@ariekanarie.nl>
2023-02-19 16:03:32 +0100

    Sort an de-dup

---
+++ doc/sort_and_dedup.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+files = ['banned_steam_ids.csv', 'banned_ips.csv']
+
+files.each do |file|
+  table = CSV.parse(File.read(file), headers: true)
+
+  puts "Sorting and de-dupping #{file}"
+
+  CSV.open(file, 'w', write_headers: true, headers: table.headers) do |csv|
+    table.uniq { |r| r[0] }.sort_by { |r| [r['name'].downcase, r['reason'], r[0]] }.each do |r|
+      csv << r
+    end
+  end
+end

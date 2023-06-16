4b9c08839ffedc33c9a23b8f35d9908ba7a51586
Arie <git@ariekanarie.nl>
2023-06-16 15:13:08 +0200

    Fix rubocop offense

--- app/models/server.rb
+++ app/models/server.rb
@@ -340,7 +340,7 @@ class Server < ActiveRecord::Base
   end
 
   def tv_port
-    self[:tv_port]&.to_i || (port.to_i + 5) # rubocop:disable Lint/RedundantSafeNavigation
+    self[:tv_port]&.to_i || (port.to_i + 5)
   end
 
   def supports_mitigations?

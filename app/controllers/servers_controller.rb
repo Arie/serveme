2f6cb91f20590a8d751056da6b38de0a815cb3fb
Arie <git@ariekanarie.nl>
2023-07-22 12:00:08 +0200

    Show update status

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -55,14 +55,24 @@ class ServersController < ApplicationController
     @server = Server.find(params[:id])
     @server.update_columns(update_status: 'Updating', update_started_at: Time.current)
     @server.restart
+    Turbo::StreamsChannel.broadcast_replace_to 'admin-server-list', target: 'admin-server-list', partial: 'servers/admin_list', locals: { servers: servers, latest_server_version: Server.latest_version }
+    head :no_content
   end
 
   def restart
     @server = Server.find(params[:id])
     @server.restart
+    Turbo::StreamsChannel.broadcast_replace_to 'admin-server-list', target: 'admin-server-list', partial: 'servers/admin_list', locals: { servers: servers, latest_server_version: Server.latest_version }
+    head :no_content
   end
 
   def permitted_params
     (params[:ssh_server] || params[:local_server]).permit(%i[name ip port rcon path active location_id])
+  end
+
+  private
+
+  def servers
+    @servers = Server.active.includes([current_reservations: { user: :groups }], :location, :recent_server_statistics).order(:name)
   end
 end

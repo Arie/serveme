38933812bd57f7d58d6aac08c02ff16cb6fdfa94
Arie <git@ariekanarie.nl>
2023-07-22 12:26:26 +0200

    Fix the query that selects servers to update

--- app/workers/server_update_worker.rb
+++ app/workers/server_update_worker.rb
@@ -21,7 +21,7 @@ class ServerUpdateWorker
 
     to_upgrade_count = MAX_CONCURRENT_UPDATES_PER_IP - currently_upgrading_count
 
-    outdated_servers.where(ip: ip).where.not(update_status: 'Updating').all.sample(to_upgrade_count).each do |s|
+    outdated_servers.where(ip: ip).where(update_status: nil).or(outdated_servers.where(ip: ip).where.not(update_status: 'Updating')).all.sample(to_upgrade_count).each do |s|
       s.update_columns(update_status: 'Updating', update_started_at: Time.current)
       s.restart
     end

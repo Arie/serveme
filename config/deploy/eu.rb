7d329e986f808225f34b003ecd52c1d49965f2c2
Arie <git@ariekanarie.nl>
2023-10-16 11:47:43 +0200

    Remove NFO api credential symlinking

--- config/deploy/eu.rb
+++ config/deploy/eu.rb
@@ -6,24 +6,3 @@ set :puma_workers,       2
 set :sidekiq_processes,  2
 set :rvm_type, :user
 server 'new.fakkelbrigade.eu', user: 'tf2', roles: %w[web app db]
-
-namespace :app do
-  desc 'symlinks the NFO servers login information'
-  task :symlink_nfoservers do
-    on roles(:web, :app) do
-      execute "rm #{release_path}/config/initializers/tragicservers.rb"
-      execute "ln -sf #{shared_path}/config/initializers/nfoservers.rb #{release_path}/config/initializers/nfoservers.rb"
-    end
-  end
-end
-after 'deploy:symlink:linked_files', 'app:symlink_nfoservers'
-
-namespace :app do
-  desc 'symlinks the simrai api information'
-  task :symlink_simrai do
-    on roles(:web, :app) do
-      execute "ln -sf #{shared_path}/config/initializers/simrai.rb #{release_path}/config/initializers/simrai.rb"
-    end
-  end
-end
-after 'deploy:symlink:linked_files', 'app:symlink_simrai'
--- config/deploy/na.rb
+++ config/deploy/na.rb
@@ -8,15 +8,3 @@ set :sidekiq_processes,  2
 set :rvm_type,           :user
 
 server 'direct.na.serveme.tf', user: 'arie', roles: %w[web app db]
-
-namespace :app do
-  desc 'symlinks the tragicservers login information'
-  task :symlink_tragicservers do
-    on roles(:web, :app) do
-      execute "ln -sf #{shared_path}/config/initializers/tragicservers.rb #{release_path}/config/initializers/tragicservers.rb"
-      execute "ln -sf #{shared_path}/config/cookie_jar.yml #{release_path}/config/cookie_jar.yml"
-    end
-  end
-end
-
-after 'deploy:symlink:linked_files', 'app:symlink_tragicservers'

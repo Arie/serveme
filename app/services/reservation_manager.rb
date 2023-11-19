582b5eeba606add11e20926fd7b2cb6ae7541a20
Arie <git@ariekanarie.nl>
2023-11-19 20:57:22 +0100

    Only look for recently uneneded reservations started before now

--- app/services/reservation_manager.rb
+++ app/services/reservation_manager.rb
@@ -37,6 +37,6 @@ class ReservationManager
   private
 
   def previous_reservation_ended_fully?
-    Reservation.where.not(id: reservation.id).where(server_id: reservation.server_id, ended: false).where('reservations.ends_at > ?', 15.minutes.ago).none?
+    Reservation.where.not(id: reservation.id).where(server_id: reservation.server_id, ended: false).where('reservations.starts_at < ? and reservations.ends_at > ?', Time.current, 15.minutes.ago).none?
   end
 end

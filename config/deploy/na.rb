f6b8cb5c481bc634a4f8fb92a5b8bf8087f8c585
Arie <git@ariekanarie.nl>
2024-01-15 14:47:58 +0100

    More threads to deal with the increased amount of servers and usage in NA

--- config/deploy/na.rb
+++ config/deploy/na.rb
@@ -2,7 +2,7 @@
 
 set :main_server,       'direct.na.serveme.tf'
 set :user,              'arie'
-set :puma_threads,      [0, 4]
+set :puma_threads,      [0, 15]
 set :puma_workers,       2
 set :sidekiq_processes,  2
 set :rvm_type,           :user

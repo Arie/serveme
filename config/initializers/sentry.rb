1f550ac24659f3cfeea814199e96944632d362c8
Arie <git@ariekanarie.nl>
2024-01-16 21:28:12 +0100

    Reduce sentry log level to WARN

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,6 +2,7 @@
 
 Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger]
+  config.logger.level = ::Logger::WARN
   config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
   config.excluded_exceptions += ['ActionController::RoutingError', 'ActiveRecord::RecordNotFound', 'Mime::Type::InvalidMimeType', 'SteamCondenser::Error::RCONNoAuth']
   config.send_default_pii = true

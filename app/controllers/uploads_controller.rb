055222b8f8c6764e67734283737cade89dcbabcf
Arie <git@ariekanarie.nl>
2024-02-03 15:17:19 +0100

    Allow admins and league admins access to all zips

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -3,16 +3,30 @@
 class UploadsController < ApplicationController
   def show
     reservation_id = params[:id]&.split('-')&.[](1)&.to_i
-    reservation = Reservation.played_in(current_user.uid).where(id: reservation_id).first
-    if reservation
-      zip_file_path = Rails.root.join('public', reservation.zipfile_name)
-      if File.exist?(zip_file_path)
-        send_file(zip_file_path)
-      else
-        head :not_found
+    respond_to do |format|
+      format.zip do
+        reservation = find_reservation(reservation_id)
+        if reservation
+          zip_file_path = Rails.root.join('public', 'uploads', reservation.zipfile_name)
+          if File.exist?(zip_file_path)
+            send_file(zip_file_path)
+          else
+            head :not_found
+          end
+        else
+          head :not_found
+        end
       end
+    end
+  end
+
+  private
+
+  def find_reservation(reservation_id)
+    if current_user.admin? || current_user.league_admin? || current_user.streamer?
+      Reservation.find(reservation_id)
     else
-      head :not_found
+      Reservation.played_in(current_user.uid).where(id: reservation_id).first
     end
   end
 end

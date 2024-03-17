76f21721ce5acc4bb43c37f2c63a3b6826866cb5
Arie <git@ariekanarie.nl>
2024-03-17 10:08:18 +0100

    Show a message won VPN signout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -85,6 +85,7 @@ class ApplicationController < ActionController::Base
         sign_out_and_redirect(current_user)
       elsif current_user.current_sign_in_ip && ReservationPlayer.banned_asn_ip?(current_user.current_sign_in_ip)
         Rails.logger.info "Logging out player on VPN with user id #{current_user.id} steam uid #{current_user.uid}, IP #{current_user.current_sign_in_ip}, name #{current_user.name}"
+        flash[:notice] = 'You appear to be on a VPN, please log in without it'
         sign_out_and_redirect(current_user)
       else
         super

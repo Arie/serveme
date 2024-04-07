4cc21a92664b36fac748971b8318a6382b1def7d
Arie <git@ariekanarie.nl>
2024-04-07 07:25:38 +0200

    Allow VPN logins for admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -83,7 +83,7 @@ class ApplicationController < ActionController::Base
       if current_user.banned?
         Rails.logger.info "Logging out banned player with user id #{current_user.id} steam uid #{current_user.uid}, IP #{current_user.current_sign_in_ip}, name #{current_user.name}"
         sign_out_and_redirect(current_user)
-      elsif current_user.current_sign_in_ip && ReservationPlayer.banned_asn_ip?(current_user.current_sign_in_ip)
+      elsif current_user.current_sign_in_ip && ReservationPlayer.banned_asn_ip?(current_user.current_sign_in_ip) && !current_user.admin?
         Rails.logger.info "Logging out player on VPN with user id #{current_user.id} steam uid #{current_user.uid}, IP #{current_user.current_sign_in_ip}, name #{current_user.name}"
         flash[:notice] = 'You appear to be on a VPN, please log in without it'
         sign_out_and_redirect(current_user)

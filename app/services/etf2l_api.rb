d66a7a9d33542c1d584542fdcf8920dd00b4fc7d
Arie <git@ariekanarie.nl>
2024-08-16 19:35:36 +0200

    Sigs for league profile fetching

--- app/services/etf2l_api.rb
+++ app/services/etf2l_api.rb
@@ -2,14 +2,19 @@
 # frozen_string_literal: true
 
 class Etf2lApi
+  extend T::Sig
+
+  sig { params(steam_uid: T.any(String, Integer)).returns(T.nilable(String)) }
   def self.profile(steam_uid)
     cached_profile_response(steam_uid) || fetch_profile(steam_uid)
   end
 
+  sig { params(steam_uid: T.any(String, Integer)).returns(T.nilable(String)) }
   def self.cached_profile_response(steam_uid)
     Rails.cache.read("etf2l_profile_#{steam_uid}")
   end
 
+  sig { params(steam_uid: T.any(String, Integer)).returns(T.nilable(String)) }
   def self.fetch_profile(steam_uid)
     response = etf2l_connection.get("player/#{steam_uid}")
     return unless response.success?
@@ -19,6 +24,7 @@ class Etf2lApi
     response.body
   end
 
+  sig { returns(Faraday::Connection) }
   def self.etf2l_connection
     Faraday.new(request: { open_timeout: 5, timeout: 5 }, url: 'https://api-v2.etf2l.org/')
   end
--- app/services/rgl_api.rb
+++ app/services/rgl_api.rb
@@ -2,14 +2,19 @@
 # frozen_string_literal: true
 
 class RglApi
+  extend T::Sig
+
+  sig { params(steam_uid: T.any(String, Integer)).returns(T.nilable(String)) }
   def self.profile(steam_uid)
     cached_profile_response(steam_uid) || fetch_profile(steam_uid)
   end
 
+  sig { params(steam_uid: T.any(String, Integer)).returns(T.nilable(String)) }
   def self.cached_profile_response(steam_uid)
     Rails.cache.read("rgl_profile_#{steam_uid}")
   end
 
+  sig { params(steam_uid: T.any(String, Integer)).returns(T.nilable(String)) }
   def self.fetch_profile(steam_uid)
     response = rgl_connection.get("v0/profile/#{steam_uid}")
     return unless response.success? || response.status == 404
@@ -19,6 +24,7 @@ class RglApi
     response.body
   end
 
+  sig { returns(Faraday::Connection) }
   def self.rgl_connection
     Faraday.new(request: { open_timeout: 5, timeout: 5 }, url: 'https://api.rgl.gg/')
   end

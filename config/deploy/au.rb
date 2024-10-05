9d9555f16135fe7a6965f8279ee64ab932998b2d
Arie <git@ariekanarie.nl>
2024-10-05 20:29:51 +0200

    Remove unused config

--- config/deploy/au.rb
+++ config/deploy/au.rb
@@ -7,6 +7,5 @@ set :puma_threads,      [0, 8]
 set :puma_workers,       0
 set :sidekiq_processes,  1
 set :rvm_type,           :user
-set :pty,                false
 
 server 'rsl.tf', user: 'arie', roles: %w[web app db]
--- config/deploy/sea.rb
+++ config/deploy/sea.rb
@@ -7,6 +7,5 @@ set :puma_threads,      [0, 4]
 set :puma_workers,       0
 set :sidekiq_processes,  1
 set :rvm_type,           :user
-set :pty,                false
 
 server '139.99.42.241', user: 'arie', roles: %w[web app db]

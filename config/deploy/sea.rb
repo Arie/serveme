3c6ec785a1af82bc7947a472937137a31d241750
Arie <git@ariekanarie.nl>
2024-10-09 13:26:23 +0200

    Update SEA configuration

--- config/deploy/sea.rb
+++ config/deploy/sea.rb
@@ -3,7 +3,7 @@
 
 set :main_server,       '139.99.42.241'
 set :user,              'arie'
-set :puma_threads,      [0, 4]
+set :puma_threads,      [1, 4]
 set :puma_workers,       0
 set :sidekiq_processes,  1
 set :rvm_type,           :user

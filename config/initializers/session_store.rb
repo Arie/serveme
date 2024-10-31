84840dd9c63b3f5d872f31cda05cb06dc4bbe4ed
Arie <git@ariekanarie.nl>
2024-10-31 09:59:24 +0100

    Cache breaker for stored sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # typed: strict
 # frozen_string_literal: true
 
-Serveme::Application.config.session_store ActionDispatch::Session::CacheStore, key: '_serveme_session', expire_after: 1.month
+Serveme::Application.config.session_store ActionDispatch::Session::CacheStore, key: '_serveme_sessions', expire_after: 1.month

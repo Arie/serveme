4eb2bf7b0f7a4540badcaae71daeef662f138687
Arie <git@ariekanarie.nl>
2024-12-02 19:16:59 +0100

    Au update

--- config/deploy/au.rb
+++ config/deploy/au.rb
@@ -1,11 +1,11 @@
 # typed: false
 # frozen_string_literal: true
 
-set :main_server,       'rsl.tf'
-set :user,              'arie'
+set :main_server,       'direct.au.serveme.tf'
+set :user,              'tf2'
 set :puma_threads,      [0, 8]
 set :puma_workers,       0
 set :sidekiq_processes,  1
 set :rvm_type,           :user
 
-server 'rsl.tf', user: 'arie', roles: %w[web app db]
+server 'direct.au.serveme.tf', user: 'tf2', roles: %w[web app db]

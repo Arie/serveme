01bd823cbff7f53ddc823f3e4b5850533e320a28
Arie <git@ariekanarie.nl>
2024-12-23 19:18:31 +0100

    Load sorbet in boot

--- config/boot.rb
+++ config/boot.rb
@@ -7,4 +7,5 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+require 'sorbet-runtime'
 require 'bootsnap/setup'

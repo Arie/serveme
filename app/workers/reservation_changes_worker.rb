b2224a4ed24488040218a694b795c957de43f33f
Vidhan Bhatt <me@vidhan.io>
2025-03-28 04:04:54 -0400

    fix: ensure `reservation.cfg` is `exec`'d if the map is changed on `PATCH` (#72)

--- app/workers/reservation_changes_worker.rb
+++ app/workers/reservation_changes_worker.rb
@@ -12,8 +12,10 @@ class ReservationChangesWorker
     if reservation.now?
       reservation.server.update_configuration(reservation)
 
-      if changes["first_map"]
-        reservation.server.rcon_exec("changelevel #{reservation.first_map}")
+      if changes["first_map"] && changes["server_config_id"]
+        reservation.server.rcon_exec("servercfgfile server.cfg; exec reservation.cfg; changelevel #{reservation.first_map}")
+      elsif changes["first_map"]
+        reservation.server.rcon_exec("servercfgfile server.cfg; changelevel #{reservation.first_map}")
       else
         reservation.server.rcon_exec("exec reservation.cfg")
       end

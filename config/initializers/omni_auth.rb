8f8e17cbf20e844102c17d03dcbfdfd9760dfc90
Arie <git@ariekanarie.nl>
2025-06-23 20:49:44 +0200

    Fix steam login on Mac OS

--- config/initializers/omni_auth.rb
+++ config/initializers/omni_auth.rb
@@ -6,4 +6,23 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 end
 
 require "openid/fetchers"
-OpenID.fetcher.ca_file = "/etc/ssl/certs/ca-certificates.crt"
+
+# Set CA certificate file path based on OS
+if Rails.env.development?
+  # macOS with Homebrew
+  if File.exist?("/opt/homebrew/etc/ca-certificates/cert.pem")
+    OpenID.fetcher.ca_file = "/opt/homebrew/etc/ca-certificates/cert.pem"
+  # macOS with MacPorts or other package managers
+  elsif File.exist?("/opt/local/etc/openssl/cert.pem")
+    OpenID.fetcher.ca_file = "/opt/local/etc/openssl/cert.pem"
+  # Linux/Ubuntu
+  elsif File.exist?("/etc/ssl/certs/ca-certificates.crt")
+    OpenID.fetcher.ca_file = "/etc/ssl/certs/ca-certificates.crt"
+  # Fallback to system default
+  else
+    Rails.logger.warn "No CA certificate file found, Steam authentication may fail"
+  end
+else
+  # Production environments (Linux)
+  OpenID.fetcher.ca_file = "/etc/ssl/certs/ca-certificates.crt"
+end

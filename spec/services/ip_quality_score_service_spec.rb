d8a421327280e68e3962b15315286839e7b9b454
Arie <git@ariekanarie.nl>
2026-01-31 11:51:43 +0100

    Remove outdated spec

--- spec/services/ip_quality_score_service_spec.rb
+++ spec/services/ip_quality_score_service_spec.rb
@@ -11,7 +11,6 @@ describe IpQualityScoreService do
   before do
     allow(Rails.application.credentials).to receive(:dig)
       .with(:ipqs, :api_key).and_return(api_key)
-    Rails.cache.clear
   end
 
   describe ".check" do
@@ -73,31 +72,6 @@ describe IpQualityScoreService do
       expect(result.is_residential_proxy).to be false
     end
 
-    it "increments usage quota" do
-      stub_request(:get, "https://www.ipqualityscore.com/api/json/ip/#{api_key}/#{ip}?strictness=1")
-        .to_return(status: 200, body: api_response.to_json)
-
-      expect(described_class.current_usage).to eq(0)
-
-      described_class.check(ip)
-
-      expect(described_class.current_usage).to eq(1)
-    end
-
-    context "when quota is exceeded" do
-      before do
-        Rails.cache.write(described_class.quota_key, 1000)
-      end
-
-      it "does not make an API request" do
-        stub = stub_request(:get, /ipqualityscore/)
-
-        expect { described_class.check(ip) }.to raise_error(IpQualityScoreService::QuotaExceededError)
-
-        expect(stub).not_to have_been_requested
-      end
-    end
-
     context "when API returns an error status" do
       it "raises ApiError on HTTP error" do
         stub_request(:get, "https://www.ipqualityscore.com/api/json/ip/#{api_key}/#{ip}?strictness=1")
@@ -113,49 +87,6 @@ describe IpQualityScoreService do
           .to_return(status: 200, body: error_response.to_json)
 
         expect { described_class.check(ip) }.to raise_error(IpQualityScoreService::ApiError, /Invalid API key/)
-      end
-    end
-  end
-
-  describe ".quota_exceeded?" do
-    it "returns false when usage is below quota" do
-      expect(described_class.quota_exceeded?).to be false
-
-      Rails.cache.write(described_class.quota_key, 999)
-      expect(described_class.quota_exceeded?).to be false
-    end
-
-    it "returns true when usage equals quota" do
-      Rails.cache.write(described_class.quota_key, 1000)
-      expect(described_class.quota_exceeded?).to be true
-    end
-  end
-
-  describe ".current_usage" do
-    it "returns 0 when no usage recorded" do
-      expect(described_class.current_usage).to eq(0)
-    end
-
-    it "returns the cached usage count" do
-      Rails.cache.write(described_class.quota_key, 42)
-      expect(described_class.current_usage).to eq(42)
-    end
-  end
-
-  describe ".quota_key" do
-    it "includes the current year and month" do
-      travel_to Time.zone.local(2026, 1, 15) do
-        expect(described_class.quota_key).to eq("ipqs_monthly_usage:2026-01")
-      end
-    end
-
-    it "changes month to month" do
-      travel_to Time.zone.local(2026, 1, 15) do
-        expect(described_class.quota_key).to include("2026-01")
-      end
-
-      travel_to Time.zone.local(2026, 2, 15) do
-        expect(described_class.quota_key).to include("2026-02")
       end
     end
   end

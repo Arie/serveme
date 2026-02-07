7e0b2b30f4e3b8aac3ae70e6b87564a41b263017
Arie <git@ariekanarie.nl>
2026-02-07 11:45:27 +0100

    Add a copy button for the rcon output

--- app/javascript/controllers/rcon_response_controller.js
+++ app/javascript/controllers/rcon_response_controller.js
@@ -1,6 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["body", "copyButton"]
+
   connect() {
     // Delay adding click-outside listener to avoid immediately dismissing
     // from the same click that submitted the form
@@ -18,6 +20,16 @@ export default class extends Controller {
 
   close() {
     this.element.remove()
+  }
+
+  copy() {
+    const text = this.bodyTarget.textContent
+    navigator.clipboard.writeText(text).then(() => {
+      this.copyButtonTarget.textContent = "\u2713"
+      setTimeout(() => {
+        this.copyButtonTarget.innerHTML = "&#x2398;"
+      }, 1500)
+    })
   }
 
   clickOutside(event) {

78814b159f2bf6a707fd431d17e740a8168ab94b
Arie <git@ariekanarie.nl>
2026-05-16 09:58:49 +0200

    Parallel specs

--- config/initializers/remote_lock.rb
+++ config/initializers/remote_lock.rb
@@ -1,4 +1,6 @@
 # typed: strict
 # frozen_string_literal: true
 
-$lock = RemoteLock.new(RemoteLock::Adapters::Redis.new(Redis.new(db: ENV.fetch("REDIS_LOCK_DB", 3).to_i)))
+test_env_number = ENV.fetch("TEST_ENV_NUMBER", "")
+worker_index = test_env_number.empty? ? 0 : test_env_number.to_i - 1
+$lock = RemoteLock.new(RemoteLock::Adapters::Redis.new(Redis.new(db: ENV.fetch("REDIS_LOCK_DB", 3).to_i + worker_index)))

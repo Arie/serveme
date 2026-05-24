cb4a19c0792287bbb8370ccc53a4b9320f6d21eb
Arie <git@ariekanarie.nl>
2026-05-24 10:28:35 +0200

    Better wording for the update banner

--- spec/views/shared/_tf2_update_banner.html.haml_spec.rb
+++ spec/views/shared/_tf2_update_banner.html.haml_spec.rb
@@ -3,13 +3,13 @@
 
 require "spec_helper"
 
-describe "shared/_docker_image_stale_banner" do
+describe "shared/_tf2_update_banner" do
   it "shows a banner when the docker image is stale" do
     allow(DockerImageReadiness).to receive(:stale?).and_return(true)
 
     render
 
-    expect(rendered).to include("temporarily unavailable")
+    expect(rendered).to include("TF2 update was just released")
   end
 
   it "renders nothing when the docker image is current" do

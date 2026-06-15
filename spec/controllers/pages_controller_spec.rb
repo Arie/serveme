4ed12404cc3e69396a1ee84eb15969f60ed58a4e
Arie <git@ariekanarie.nl>
2026-06-15 22:23:33 +0200

    Render welcome via v2 variant and vary action cache by ui_v2

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -52,4 +52,39 @@ describe PagesController do
       response.status.should == 500
     end
   end
+
+  describe "#welcome redesign gating" do
+    it "renders the v2 layout + template when opted in" do
+      request.cookies["ui_v2"] = "true"
+      get :welcome
+      expect(response).to render_template("layouts/application_v2")
+      expect(response).to render_template("pages/welcome")
+    end
+
+    it "renders the default layout when not opted in" do
+      cookies.delete(:ui_v2) if respond_to?(:cookies)
+      get :welcome
+      expect(response).not_to render_template("layouts/application_v2")
+    end
+  end
+
+  describe "#welcome caching" do
+    around do |example|
+      caching = ActionController::Base.perform_caching
+      previous_store = Rails.cache
+      ActionController::Base.perform_caching = true
+      Rails.cache = ActiveSupport::Cache::MemoryStore.new
+      example.run
+      ActionController::Base.perform_caching = caching
+      Rails.cache = previous_store
+    end
+
+    it "does not serve the cached classic home to opted-in logged-out users" do
+      sign_out @user
+      get :welcome          # primes the classic cache entry
+      request.cookies["ui_v2"] = "true"
+      get :welcome          # different cache key -> miss -> renders v2
+      expect(response).to render_template("layouts/application_v2")
+    end
+  end
 end
